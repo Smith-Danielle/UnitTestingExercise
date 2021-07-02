b087c93025b755554c7f5fe38490d0e3a7cb57b6
Smith-Danielle <smith.ddms@gmail.com>
2021-07-02 09:34:42 -0500

    Completed TDD Exercise 1

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,50 +6,62 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(2, 2, 3, 7)]
+        [InlineData(-1, 3, 5, 7)]
+        [InlineData(10, 50, 0, 60)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.AddTool(num1, num2, num3);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2, 0, 2)]
+        [InlineData(-1, 10, -11)]
+        [InlineData(60, 15, 45)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.SubtractTool(minuend, subtrhend);
             //Assert
+            Assert.Equal(actual, expected);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2, 2, 4)]
+        [InlineData(0, 10, 0)]
+        [InlineData(-10, 5, -50)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.MultiplyTool(num1, num2);
             //Assert
+            Assert.Equal(actual, expected);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10, 5, 2)]
+        [InlineData(0, 25, 0)]
+        [InlineData(-20, 4, -5)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.DivideTool(num1, num2);
             //Assert
+            Assert.Equal(actual, expected);
 
         }
 
@@ -57,21 +69,22 @@ namespace UnitTestingExercise.Tests
         public void YourMethodName()
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.SayHello();
             //Assert
-
+            Assert.Equal("Hello", actual);
         }
 
         [Fact]
         public void YourMethodName2()
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.PlusMinus(2, 4, 1);
             //Assert
+            Assert.Equal(5, actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -9,18 +9,47 @@ namespace UnitTestingExercise
 
 
         // Create an Add method that passes 3 integers
+        public int AddTool(int numOne, int numTwo, int numThree)
+        {
+            int answer = numOne + numTwo + numThree;
+            return answer;
+        }
 
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public int SubtractTool(int numOne, int numTwo)
+        {
+            int answer = numOne - numTwo;
+            return answer;
+        }
 
         // Create a Multiply method that passes 2 integers
+        public int MultiplyTool(int numOne, int numTwo)
+        {
+            int answer = numOne * numTwo;
+            return answer;
+        }
 
         // Create a Divide method that passes 2 integers
-
+        public int DivideTool(int numOne, int numTwo)
+        {
+            int answer = numOne / numTwo;
+            return answer;
+        }
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public string SayHello()
+        {
+            return "Hello";
+        }
+        public int PlusMinus(int numOne, int numTwo, int numThree)
+        {
+            int answer = numOne + numTwo - numThree;
+            return answer;
+        }
 
-        
+
+
     }
 }
